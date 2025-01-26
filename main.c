79b7e4b2d11955af2daf9de4848c808dd7fc5323
romuald-c <romuald.gorda@gmail.com>
2025-01-26 23:34:49 +0000

    second commit

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(void)
     printf("\n\n");
     */
 
-    InitWindow(1280, 720, "Hello Romuald");      //opening the window (size 1280x720) with name 'Hello Romuald'
+    InitWindow(1280, 720, "Lucky Miner!");      //opening the window (size 1280x720) with name 'Hello Romuald'
     SetTargetFPS(60);                           //Refresh rate (60 Frames Per Second)
 
     //-------Start the Game main Loop---------------
@@ -16,7 +16,7 @@ int main(void)
     {
         BeginDrawing();                         //From this point we begin to draw.
         ClearBackground(DARKGRAY);              // Setting the background color to  dark gray 
-        DrawText("Hello Louie!", 140, 30, 20, LIGHTGRAY);   //Writting the text in the windows 'Hello Louie!' position in the window and the color
+        DrawText("Welcome to \"Lucky Miner\" Game!", 140, 30, 20, RED);   //Writting the text in the windows 'Hello Louie!' position in the window and the color
         EndDrawing();                           //After this point there is no more drawing.
     }
     CloseWindow();                              //Closing the window.
