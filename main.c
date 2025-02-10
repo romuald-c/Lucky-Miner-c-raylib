149995cad2e37e01c890dd6792601c088f4b7035
romuald-c <romuald.gorda@gmail.com>
2025-02-10 21:41:53 +0000

    Edge detection added

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <raylib.h>
 
-int screenX = 1600;
-int screenY = 900;
+int screenX = 1280;
+int screenY = 720;
 
 int main(void)
 {
@@ -21,22 +21,44 @@ int main(void)
     while(!WindowShouldClose())
     {
         // moovement of the circle.
-        if(IsKeyDown(KEY_UP))
+        if (IsKeyDown(KEY_RIGHT) && IsKeyDown(KEY_UP) && circCentreX <= screenX - 30 && circCentreY >= 30)
+        {
+            circCentreX += speed;
+            circCentreY -= speed;
+        }
+        else if (IsKeyDown(KEY_RIGHT) && IsKeyDown(KEY_DOWN) && circCentreX <= screenX - 30 && circCentreY <= screenY - 30)
+        {
+            circCentreX += speed;
+            circCentreY += speed;
+        }
+        else if (IsKeyDown(KEY_LEFT) && IsKeyDown(KEY_UP) && circCentreX >= 30 && circCentreY >= 30)
+        {
+            circCentreX -= speed;
+            circCentreY -= speed;
+        }
+        else if (IsKeyDown(KEY_LEFT) && IsKeyDown(KEY_DOWN) && circCentreX >= 30 && circCentreY <= screenY - 30)
+        {
+            circCentreX -= speed;
+            circCentreY += speed;
+        }
+
+        else if(IsKeyDown(KEY_UP) && circCentreY >= 30)
         {
             circCentreY -= speed;
         }
-        else if (IsKeyDown(KEY_DOWN))
+        else if (IsKeyDown(KEY_DOWN) && circCentreY <= screenY - 30)
         {
             circCentreY += speed;
         }
-        else if (IsKeyDown(KEY_LEFT))
+        else if (IsKeyDown(KEY_LEFT) && circCentreX >= 30)
         {
             circCentreX -= speed;
         }
-        else if (IsKeyDown(KEY_RIGHT))
+        else if (IsKeyDown(KEY_RIGHT) && circCentreX <= screenX - 30)
         {
             circCentreX += speed;
         }
+
 
         // Size of the circle.
         if(IsKeyDown(KEY_Z))
@@ -52,11 +74,11 @@ int main(void)
                                  
         ClearBackground(DARKGRAY);              // Setting the background color to  dark gray 
         
-        DrawRectangle(1000, 300, 500, 80, MAGENTA);
+        DrawRectangle(700, 300, 500, 80, MAGENTA);
 
         DrawCircle(circCentreX, circCentreY, circSize, GREEN);
 
-        DrawText("Welcome to \"Lucky Miner\" Game!", 100, 300, 80, RED);   //Writting the text in the windows 'Welcome.....' position in the window and the color
+        DrawText("Welcome to \"Lucky Miner\" Game!", 30, 100, 50, RED);   //Writting the text in the windows 'Welcome.....' position in the window and the color
         
         
         EndDrawing();                           //After this point there is no more drawing.
